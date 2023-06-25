4a36127b0423bfe86a1f8594a13e5b58659234c5
Jingshi Liu <jingshiliu07@gmail.com>
2023-06-24 20:47:20 -0400

    finish Section 3: Type Annotation

--- Section 3: Type Annotation Basics/variable.ts
+++ Section 3: Type Annotation Basics/variable.ts
@@ -13,3 +13,50 @@ numCatLives ++
 
 let gameOver: boolean = false
 console.log(movieTitle)
+
+
+// Type Inference
+// Typescript can figure out the primitive types of variable
+
+let movie = 'Avengers'
+movie = 'Iron Man'
+movie = 1
+
+
+
+// Any Type
+// basically saying do not check type for it
+let thing: any = 'hello'
+thing = 1
+thing = true
+thing()
+
+let anotherThing = 'hello'
+anotherThing.crAts()
+
+
+// implicit any type
+
+const movies = ['Arrival', 'The Thing', 'Aliens']
+let foundMovie // Implicit Any
+
+for(let theMovie of movies){
+    if (theMovie === 'Arrival')
+        theMovie = 'Arrival' // Type Any
+}
+
+foundMovie() // no error 
+
+// -------------
+// We should do this
+
+const movies1 = ['Arrival', 'The Thing', 'Aliens']
+let foundMovie1: string // Type String
+
+for(let theMovie of movies){
+    if (theMovie === 'Arrival')
+        theMovie = 'Arrival' // Still string
+}
+
+foundMovie1() // error
+
